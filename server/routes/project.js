82eab5faf061c184b61e85c60a9c1620897fb451
Ahmed El-Haddad <ahalhdad2023@gmail.com>
2026-02-12 23:51:50 +0200

    Check the code and make some fixes

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,8 +1,18 @@
 import express from "express";
 import verifyToken from "../middlewares/verifyToken.js";
-import { create } from "../controllers/project.js";
+import {
+  create,
+  edit,
+  get,
+  love,
+  remove,
+  unLove,
+} from "../controllers/project.js";
 const router = express.Router();
 router.post("/new", create);
-router.put("/new", verifyToken, create);
-router.post("/new", verifyToken, create);
+router.get("/:id", get);
+router.put("/:id", verifyToken, edit);
+router.delete("/:id", remove);
+router.post("/love/:id", verifyToken, love);
+router.post("/unlove/:id", verifyToken, unLove);
 export default router;
